8a04517ec8d814347c3e1692b7a41da0cb92caa5
Abhishek Kumar Rai <75179538+abhishek00210@users.noreply.github.com>
2024-10-20 06:29:30 +0530

    Update teamRoutes.js

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createTeam, getTeamById } = require('../controllers/teamController');
 const router = express.Router();
 
-router.post('/teams', createTeam);
+router.post('https://fantansy-game-backend-1.onrender.com/api/teams', createTeam);
 router.get('/teams/:id', getTeamById);
 
 module.exports = router;
